ed74762eef159cc26bc38d2516c1edb50570b2c7
MSI\hnafF <hnaf.office@gmail.com>
2025-08-03 18:15:52 +0900

    feat:disable ESlint and add export alias

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
