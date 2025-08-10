8a2e73cedd14bdaaefc8389f781c2869d14b7726
MSI\hnafF <hnaf.office@gmail.com>
2025-08-10 11:34:02 +0900

    refactor: upgrade footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,20 +1,14 @@
 'use client';
 
 import { useLang } from '@/i18n/LanguageProvider';
+import '@/styles/components/nav/footer.css';
 
 export default function Footer() {
   const { t } = useLang();
 
   return (
-    <footer
-      className="text-center py-4"
-      style={{
-        backgroundColor: 'var(--footer-transparent)',
-        color: 'var(--foreground)',
-        transition: 'var(--transition)',
-      }}
-    >
-      {t('components.footer')}
+    <footer className="footer">
+      <p>{t('components.footer')}</p>
     </footer>
   );
 }
