1c9e5d99a7edb1726a0e42e00e104c625c313748
MSI\hnafF <hnaf.office@gmail.com>
2025-08-15 20:52:03 +0900

    feat: fix footer bug

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -4,11 +4,14 @@ import { useLang } from '@/i18n/LanguageProvider';
 import '@/styles/components/nav/footer.css';
 
 export default function Footer() {
-  const { t } = useLang();
+  const { t, lang } = useLang();
+
+  if (!lang) return null; // 等語言載入後再渲染
 
   return (
     <footer className="footer">
-      <p>{t('components.footer')}</p>
+      {/* <p>{t('components.footer')}</p> */}
+      <p>© 2025 Quatre</p>
     </footer>
   );
 }
